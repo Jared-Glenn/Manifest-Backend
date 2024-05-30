01e38ecc76a1f3ccf1fbab9c3a7fe604a0aa5051
Jared <jared_glenn@yahoo.com>
2024-05-29 23:31:43 -0600

    add register function

--- models/user.js
+++ models/user.js
@@ -6,7 +6,45 @@ const { BadRequestError, NotFoundError, UnauthorizedError, } = require("../expre
 const { BCRYPT_WORK_FACTOR } = require("../config.js");
 
 class User {
+    static async register(
+    { username, password, firstName, lastName, email, state }) {
+    const duplicateCheck = await db.query(
+        `SELECT username
+        FROM users
+        WHERE username = $1`,
+        [username],
+    )
 
+    if(duplicateCheck.rows[0]) {
+        throw new BadRequestError(`Duplicate username: ${username}`);
+    }
+
+    const hashedPassword = await BCRYPT_WORK_FACTOR.has(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+        `INSERT INTO users
+        (username,
+            password,
+            first_name,
+            last_name,
+            email,
+            state)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING username, first_name as "firstName", last_name AS "lastName", email, state`,
+        [
+            username,
+            hashedPassword,
+            firstName,
+            lastName,
+            email,
+            state,
+        ],
+    );
+
+    const user = result.rows[0];
+    
+    return user;
+    }
 }
 
 module.exports = User;
